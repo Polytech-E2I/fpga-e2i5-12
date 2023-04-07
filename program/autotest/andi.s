e1ae2c5aa54f9d98b7c3660b8aba3fb179be0228
Robin SHAMSNEJAD <rsham@free.fr>
2023-04-07 10:59:11 +0200

    Add ANDI

---
+++ program/autotest/andi.s
@@ -0,0 +1,11 @@
+# TAG = andi
+    .text
+
+    addi x31,   zero,   -1
+    andi x31,   x31,    0x74C
+
+    # max_cycle 50
+    # pout_start
+    # FFFFFFFF
+    # 0000074C
+    # pout_end
--- vhd/CPU_PC.vhd
+++ vhd/CPU_PC.vhd
@@ -374,6 +374,18 @@ begin
                 -- next state
                 state_d         <= S_Fetch;
 
+            when S_ANDI =>
+                -- rd <- rs1 & rs2
+                cmd.ALU_Y_sel       <= ALU_Y_immI;
+                cmd.LOGICAL_op      <= LOGICAL_and;
+                cmd.RF_we           <= '1';
+                cmd.DATA_sel        <= DATA_from_logical;
+                -- lecture mem[PC]
+                cmd.ADDR_sel    <= ADDR_from_pc;
+                cmd.mem_ce      <= '1';
+                cmd.mem_we      <= '0';
+                -- next state
+                state_d         <= S_Fetch;
 
 ---------- Instructions de saut ----------
 

b85f06253bd27cf2481492042c50781d82fd9783
Robin SHAMSNEJAD <rsham@free.fr>
2023-04-07 11:48:37 +0200

    Replace tabs in autotests

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -1,13 +1,13 @@
 # TAG = addi
-	.text
+    .text
 
-	addi x31, zero, 0x037
-	addi x31, zero, -1
-	addi x31, x31,  22
+    addi x31,   zero,   0x037
+    addi x31,   zero,   -1
+    addi x31,   x31,    22
 
-	# max_cycle 50
-	# pout_start
-	# 00000037
-	# FFFFFFFF
-	# 00000015
-	# pout_end
+    # max_cycle 50
+    # pout_start
+    # 00000037
+    # FFFFFFFF
+    # 00000015
+    # pout_end

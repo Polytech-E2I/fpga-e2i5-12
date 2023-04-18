d542e1f48b1b8b34b86a8ec437d96383594f6f3c
Robin SHAMSNEJAD <rsham@free.fr>
2023-04-18 09:30:06 +0200

    Update SLT test

--- program/autotest/slt.s
+++ program/autotest/slt.s
@@ -2,12 +2,14 @@
     .text
 
     addi t0,    zero,   7
-    addi t1,    zero,   4
+    addi t1,    zero,   -4
     slt  x31,   t0,     t1
     slt  x31,   t1,     t0
+    slt  x31,   t1,     t1
 
     # max_cycle 50
     # pout_start
     # 00000001
+    # 00000000
     # 00000000
     # pout_end

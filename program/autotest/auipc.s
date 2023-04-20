352073a60318cd14857d483f810eb2f24d0d50f8
Liliana Andrade <liliana.andrade@univ-grenoble-alpes.fr>
2023-04-20 22:42:38 +0200

    Updating auipc test - operation to be fixed

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -3,9 +3,9 @@
 
     # Upon decoding this instruction:
     # pc = 0x00001000
-    auipc   x31,    5
+    auipc   x31,    0
 
     # max_cycle 50
     # pout_start
-    # 00006004
+	# 00001000
     # pout_end

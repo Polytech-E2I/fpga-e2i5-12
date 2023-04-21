861c11b8eb6516186520119b17102ff0a34b6393
Robin SHAMSNEJAD <rsham@free.fr>
2023-04-21 12:11:47 +0200

    Try to fix chenillard (WIP)

--- program/chenillard_minimaliste.s
+++ program/chenillard_minimaliste.s
@@ -1,32 +1,34 @@
 .text
 
-    # index <> x11 = 24
-    addi    x11,    zero,   20
+    # index <> x1 = 24
+    addi    x1,    zero,   3
 
-    # mask <> x12 = 1 << index
-    addi    x12,    zero,   1
-    sll     x12,    x12,    x11
+    # mask <> x2 = 1 << index
+    addi    x2,    zero,   1
+    sll     x2,    x2,    x1
 
-    # compteur <> x13 = compteur + 1
-    addi    x13,    x13,    1
+    # compteur <> x3 = compteur + 1
+    addi    x3,    x3,    1
 
-    # bit_extrait <> x14 = compteur & mask
-    and     x14,    x13,    x12
+    # bit_extrait <> x4 = compteur & mask
+    and     x4,    x3,    x2
 
-    # change <> x15 = bit_extrait xor bit_extrait_precedent
-    xor     x15,    x14,    x16
+    # change <> x5 = bit_extrait xor bit_extrait_precedent
+    xor     x5,    x4,    x6
 
-    # bit_extrait_precedent <> x16 = bit_extrait
-    addi    x16,    x15,    0
+    # bit_extrait_precedent <> x6 = bit_extrait
+    addi    x6,    x5,    0
 
     # change = change >> index
-    srl     x15,    x15,    x11
+    srl     x5,    x5,    x1
 
-    # motif <> x17 = motif << change
-    sll     x17,    x17,    x15
+    # motif <> x7 = motif << change
+    sll     x7,    x7,    x5
 
     # motif = motif or 0x1
-    ori     x17,    x17,    0x1
+    ori     x7,    x7,    0x1
 
     # afficher(motif)
-    addi    x31,    x17,    0
+    addi    x31,    x7,    0
+
+    #mul     x0,     x0,     x0

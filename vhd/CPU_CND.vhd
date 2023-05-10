3c0a0db70fafbaf84e9b3ad75f049ee07dd3ac37
Robin SHAMSNEJAD <rsham@free.fr>
2023-05-10 15:19:14 +0200

    Add CPU_CND (signext TODO)

--- vhd/CPU_CND.vhd
+++ vhd/CPU_CND.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.PKG.all;
 
+
 entity CPU_CND is
     generic (
         mutant      : integer := 0
@@ -17,42 +18,44 @@ entity CPU_CND is
 end entity;
 
 architecture RTL of CPU_CND is
-    -- signal sign_ext : std_logic;
-    -- signal z        : std_logic;
-    -- signal s        : std_logic;
+
+    subtype w33   is unsigned(32 downto 0);
+
+    signal sign_ext : std_logic;
+    signal z        : std_logic;
+    signal s        : std_logic;
+    signal sum      : w33;
 
 begin
 
-    -- process(IR, rs1, alu_y) is
-    -- begin
-    --     sign_ext <= (IR(6) and not IR(13)) or (IR(12) nor IR(6)) ;
-    --     if rs1 = alu_y then
-    --         z <= '1';
-    --     else
-    --         z <= '0';
-    --     end if;
+    CPU_CND: process(IR, rs1, alu_y) is
+    begin
 
-    --     if sign_ext and IR(31) then
-    --         if rs1 > alu_y then
-    --             s <= '1';
-    --         else
-    --             s <= '0';
-    --         end if;
-    --     else
-    --         if rs1 < alu_y then
-    --             s <= '1';
-    --         else
-    --             s <= '0';
-    --         end if;
-    --     end if;
+        if rs1 = alu_y then
+            z <= '1';
+        else
+            z <= '0';
+        end if;
 
-    --     jcond <= (not IR(14) and (IR(12) xor (z))) or (IR(14) and (IR(12) xor s));
-    --     slt <= s;
-    -- end process;
+        sum <= rs1 + alu_y;
 
+        s <= sum(32);
 
+        sign_ext <= ( ( not IR(12) ) and ( not IR(6) ) )
+                        or
+                    ( IR(6) and ( not IR(13) ) )
+        ;
 
-    jcond <= '0';
-    slt <= '0';
+        jcond <=    ( ( not IR(14) ) and ( IR(12) xor z ) )
+                        or
+                    ( IR(14) and ( IR(12) xor s ) )
+        ;
+
+        slt <= s;
+
+    end process;
+
+    -- jcond <= '0';
+    -- slt <= '0';
 
 end architecture;

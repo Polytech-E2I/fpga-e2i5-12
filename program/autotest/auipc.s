549d77ee471bf3f5796533a74f6c7402fe3aec59
Liliana Andrade <liliana.andrade@univ-grenoble-alpes.fr>
2023-06-06 11:04:20 +0200

    AUIPC test prof

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -1,11 +1,21 @@
 # TAG = auipc
-    .text
+	.text
 
-    # Upon decoding this instruction:
-    # pc = 0x00001000
-    auipc   x31,    0
+	auipc x31, 0       # Test sur la valeur 0
 
-    # max_cycle 50
-    # pout_start
+	auipc x31, 0xFFFFF # Test sur la valeur maximal sur 20 bits
+
+	auipc x31, 0x12345 # Test sur une valeur sur 20 bits
+
+	auipc x31, 0x7FFFF # Test sur la valeur maximal positive sur 20 bits
+
+	auipc x31, 0x80000 # Test en soustrayant de l'adresse de base du code
+
+	# max_cycle 50
+	# pout_start
 	# 00001000
-    # pout_end
+	# 00000004
+	# 12346008
+	# 8000000C
+	# 80001010
+	# pout_end
